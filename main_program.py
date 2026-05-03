91673f02c676019f1e27e453ff4d7954f0eb0a69
jlnshly <jelaineashleyramos@gmail.com>
2026-05-03 18:31:43 +0800

    adding main program calculations and user input

--- main_program.py
+++ main_program.py
@@ -10,4 +10,16 @@ class MainProgram(MathOperations):
 
     def calculator():
         while True:
-            print()
+            print("Welcome to Python Simple Calculator")
+            print("Please choose the operation:")
+            print("1. Addition")
+            print("2. Subtraction")
+            print("3. Multiplication")
+            print("4. Division")
+            user_choice = str(input("Please choose the operation from 1-4: "))
+            if user_choice in ("1", "2", "3", "4"):
+                try:
+                    first_number = float(input("Please enter the first number: "))
+                    second_number = float(input("Please enter the second number: "))
+                    if user_choice == "1":
+                        print()
