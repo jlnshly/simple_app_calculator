482c1f09cead19c0b98f12c403954b14fb7af42b
jlnshly <jelaineashleyramos@gmail.com>
2026-05-03 18:21:20 +0800

    complete adding 4 required math operations

---
+++ math_operations.py
@@ -0,0 +1,9 @@
+class MathOperations:
+    def addition(self, a, b):
+        return a + b
+    def subtraction(self, a, b):
+        return a - b
+    def multiplication(self, a, b):
+        return a * b
+    def division(self, a, b):
+        return a / b
